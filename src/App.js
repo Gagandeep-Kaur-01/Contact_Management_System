5e2b2ffbed53d1b6d2c05eb6cfd93354e0d41f77
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-08 21:45:40 +0530

    created 'App.js' as function component and imported it

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+import './App.css';
+
+function App() {
+  
+  return (
+    <Fragment>
+
+        <h2> Function component</h2>
+
+    </Fragment>   
+    
+  );
+}
+
+export default App;
