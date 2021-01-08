e20191f97d2835db4eb8a792a0421d5d1c83814a
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-08 21:53:32 +0530

    created event handlers

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import { PlusCircleFilled } from "@ant-design/icons"
 
 function App() {
     const [visible, setVisible] = useState(false);
+
+    const showDrawer = () => {
+        setVisible(true);
+      }
+    
+      const onClose = () => {
+        setVisible(false);
+      };
   
   return (
     <Fragment>
 
       <Button type="primary" 
-              icon={<PlusCircleFilled/>} 
+              icon={<PlusCircleFilled/>}
+              onClick={showDrawer} 
       >
         Add Contact
       </Button>
