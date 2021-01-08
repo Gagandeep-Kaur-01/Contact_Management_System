51c7907f7235b642c0d626319b777283652b1b47
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-08 21:52:05 +0530

    used 'useState' (state hook) to have state variables

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import './App.css';
 import { Button } from 'antd'
 import { PlusCircleFilled } from "@ant-design/icons"
 
 function App() {
+    const [visible, setVisible] = useState(false);
   
   return (
     <Fragment>
