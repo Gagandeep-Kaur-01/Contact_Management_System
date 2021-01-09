ee77e8661fe993b4b016ba7f6510b8fd4d92d488
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-09 21:28:46 +0530

    created separate 'Sidebar' component

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { Layout, Menu } from 'antd'
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+function Sidebar(){
+    const [collapsed, setCollapsed] = useState(false);
+
+    const onCollapse = (isCollapsed) => {
+        setCollapsed(isCollapsed);
+      };
+
+      return(
+        <Layout style={{ minHeight: '100vh' }}>
+        <Sider 
+          collapsible 
+          collapsed={collapsed} 
+          onCollapse={onCollapse}
+         >
+    
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+           
+           {/*....................*/}
+          
+            </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+  </Layout>    
+      )
+
+}
+
+export default Sidebar;
