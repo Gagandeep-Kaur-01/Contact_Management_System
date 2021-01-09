719d3136976a830bce1416d884083eb0862a354a
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-09 22:11:38 +0530

    updated store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
-import { createStore } from "redux";
+import { createStore, combineReducers, applyMiddleware, } from "redux";
+import contactReducer from "./contacts/reducer";
+import thunk from 'redux-thunk';
 
 const store = createStore(
-    
+    combineReducers({
+        contacts: contactReducer,
+    }),
+    applyMiddleware(thunk)
 );    
 
 export default store;
