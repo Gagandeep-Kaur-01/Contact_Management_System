0fdd969f42c5ca11bf56b6af0258b9730e974320
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-09 21:18:35 +0530

    created separate form component for add contact

--- src/Components/AddDrawer.js
+++ src/Components/AddDrawer.js
@@ -1,21 +1,10 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import 'antd/dist/antd.css';
-import { Drawer, Form, Input, Button } from "antd";
+import { Drawer } from "antd";
+import AddForm from './Form'
 
-const AddDrawer = ({
-  show, 
-  handleonClose, 
-  handleOnFinish, 
-  handleOnFinishFailed}) => {
-    const initialValues = { firstName: "", lastName: "", phoneNumber: null };
-    const [form] = Form.useForm();
-    const [, forceUpdate] = useState({});
-
-  // To disable submit button at the beginning.
-  useEffect(() => {
-    forceUpdate({});
-  }, []);
+const AddDrawer = ({ show, handleonClose, handleOnFinish, handleOnFinishFailed}) => {
   
     return (
       <Drawer 
@@ -25,59 +14,9 @@ const AddDrawer = ({
         onClose={handleonClose}
         visible={show}
        >
-         <Form
-            form={form}
-            name="basic"
-            initialValues={initialValues}
-            onFinish={handleOnFinish}
-            onFinishFailed={handleOnFinishFailed}
-            layout="vertical"
-          >
-            <Form.Item
-              label="First Name"
-              name="firstName"
-              rules={[{ required: true, message: 'Please input your first name!' }]}
-            >
-              <Input />
-            </Form.Item>
-
-            <Form.Item
-              label="Last Name"
-              name="lastName"
-              rules={[{ required: true, message: 'Please input your last name!' }]}
-            >
-              <Input />
-            </Form.Item>
-
-            <Form.Item
-              label="Phone Number"
-              name="phoneNumber"
-              rules={[{ required: true, message: 'Please input your phone number!' }]}
-            >
-              <Input type="tel" />
-            </Form.Item>
-               
-              <Form.Item shouldUpdate >
-              {() => (
-                <Fragment>
-                  <Button
-                    style={{ marginRight: '20px'}}
-                    type="primary"
-                    htmlType="submit"
-                    disabled={
-                      !form.isFieldsTouched(true) ||
-                      !!form.getFieldsError().filter(({ errors }) => errors.length).length
-                    }
-                   >
-                    Add
-                  </Button>
-                  <Button htmlType="submit" onClick= {() => form.resetFields()} >
-                    Reset
-                  </Button> 
-                </Fragment>
-              )}
-              </Form.Item>
-          </Form>
+         <AddForm 
+          onFinish={handleOnFinish}
+          onFinishFailed={handleOnFinishFailed}/>        
       </Drawer>
     );      
 };
---
+++ src/Components/Form.js
@@ -0,0 +1,69 @@
+import React, { useState, Fragment } from 'react';
+import { Form, Input, Button } from 'antd';
+
+
+function Forms({ onFinish, onFinishFailed }) {
+    
+  const initialValues = { firstName: "", lastName: "", phoneNumber: null };
+  const [form] = Form.useForm(); 
+  const [, forceUpdate] = useState({});
+    
+    return(
+      <Form
+        form={form}
+        name="basic"
+        initialValues={initialValues}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        layout="vertical"
+       >
+        <Form.Item
+          label="First Name"
+          name="firstName"
+          rules={[{ required: true, message: 'Please input your first name!' }]}
+         >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Last Name"
+          name="lastName"
+          rules={[{ required: true, message: 'Please input your last name!' }]}
+         >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Phone Number"
+          name="phoneNumber"
+          rules={[{ required: true, message: 'Please input your phone number!' }]}
+         >
+          <Input type="tel" />
+        </Form.Item>
+             
+        <Form.Item shouldUpdate >
+          {() => (
+            <Fragment>
+              <Button
+                style={{ marginRight: '20px'}}
+                type="primary"
+                htmlType="submit"
+                disabled={
+                  !form.isFieldsTouched(true) ||
+                  !!form.getFieldsError().filter(({ errors }) => errors.length).length
+                }
+               >
+                Add
+              </Button>
+              <Button htmlType="submit" onClick= {() => form.resetFields()} >
+                Reset
+              </Button> 
+            </Fragment>
+          )}
+        </Form.Item>
+      </Form>
+    )
+
+}    
+
+export default Forms;
