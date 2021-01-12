4f3bc0ab2891962338603aada2e9df62b9e0e621
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-12 21:51:24 +0530

    created action for 'edit contacts'

--- src/redux/contacts/actions.js
+++ src/redux/contacts/actions.js
@@ -1,9 +1,18 @@
-import { ADD_CONTACT, DELETE_CONTACT } from "./actionTypes"
+import { ADD_CONTACT, DELETE_CONTACT, EDIT_CONTACT } from "./actionTypes"
 
 export const addContact = (contact) => {
     return (dispatch) => {
         dispatch ({
             type: ADD_CONTACT,
+            payload: contact,
+        });
+    };
+};
+
+export const editContact = (contact) => {
+    return (dispatch) => {
+        dispatch ({
+            type: EDIT_CONTACT,
             payload: contact,
         });
     };
